1b373589dda04d7b26d66594cc2f71e6605a86da
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 04:37:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday|
-    info.collect do |holiday, supplies|
+    holiday.collect do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
   end

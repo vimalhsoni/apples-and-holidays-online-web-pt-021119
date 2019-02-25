b26ea577af8549861950d81e00d84096187958f6
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 04:26:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, info|
-  puts "season.to_s.capitalize!}:"
+  puts "#{season.to_s.capitalize!}:"
   info.each do |holiday, supplies|
     str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
     puts "  #{str_holiday}: #{supply.join(", ")}"
